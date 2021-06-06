c4b67774abcc540ceb68816f5fde5a1050c32ead
Juseong Park <juseongkr@gmail.com>
2021-06-06 11:18:54 +0900

    Create: command ls

--- filesystem/src/files/DirectoryEntry.scala
+++ filesystem/src/files/DirectoryEntry.scala
@@ -3,4 +3,5 @@ package files
 abstract class DirectoryEntry(val parentPath: String, val name: String) {
   def path: String = parentPath + Directory.SEPARATOR + name
   def toDirectory: Directory
+  def getType: String
 }
