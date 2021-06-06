bc94f771f55d020132b1d42598827e8ac646d535
Juseong Park <juseongkr@gmail.com>
2021-06-06 11:09:45 +0900

    Create: mkdir commands (addEntry)

--- filesystem/src/files/Directory.scala
+++ filesystem/src/files/Directory.scala
@@ -1,8 +1,35 @@
 package files
 
+import scala.annotation.tailrec
+
 class Directory(override val parentPath: String, override val name: String, val contents: List[DirectoryEntry])
   extends DirectoryEntry(parentPath, name) {
-  def hasEntry(name: String): Boolean = ???
+  def hasEntry(name: String): Boolean = findEntry(name) != null
+
+  def getAllFoldersInPath: List[String] =
+    path.substring(1).split(Directory.SEPARATOR).toList.filter(x => x.nonEmpty)
+
+  def findDescendant(path: List[String]): Directory = {
+    if (path.isEmpty) this
+    else findEntry(path.head).toDirectory.findDescendant(path.tail)
+  }
+
+  def addEntry(newEntry: DirectoryEntry): Directory = new Directory(parentPath, name, contents :+ newEntry)
+
+  def findEntry(entryName: String): DirectoryEntry = {
+    @tailrec
+    def findEntryHelper(name: String, contentList: List[DirectoryEntry]): DirectoryEntry = {
+      if (contentList.isEmpty) null
+      else if (contentList.head.name.equals(name)) contentList.head
+      else findEntryHelper(name, contentList.tail)
+    }
+    findEntryHelper(entryName, contents)
+  }
+
+  def replaceEntry(entryName: String, newEntry: DirectoryEntry): Directory =
+    new Directory(parentPath, name, contents.filter(x => !x.name.equals(entryName)) :+ newEntry)
+
+  def toDirectory: Directory = this
 }
 
 object Directory {
--- filesystem/src/files/DirectoryEntry.scala
+++ filesystem/src/files/DirectoryEntry.scala
@@ -1,5 +1,6 @@
 package files
 
 abstract class DirectoryEntry(val parentPath: String, val name: String) {
-
+  def path: String = parentPath + Directory.SEPARATOR + name
+  def toDirectory: Directory
 }
